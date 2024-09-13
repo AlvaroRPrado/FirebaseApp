3c641fd45065b2678434f043d171b4ecfeaeb557
AlvaroRPrado <alvarorodrigues2010@gmail.com>
2024-09-12 21:36:59 -0300

    Como recuperar dados

--- app/src/main/java/com/prado/firebase/MainActivity.java
+++ app/src/main/java/com/prado/firebase/MainActivity.java
@@ -1,15 +1,22 @@
 package com.prado.firebase;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,16 +32,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        DatabaseReference usuarios = reference.child("usuarios");
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE", Objects.requireNonNull(dataSnapshot.getValue()).toString());
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+        DatabaseReference usuarios2 = reference.child("usuarios").child("001");
+        usuarios2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIRE", Objects.requireNonNull(snapshot.getValue()).toString());
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         //reference.child("pontos").setValue("300");
        // reference.child("usuarios2").child("001").child("nome").setValue("Priscila");
-        DatabaseReference produtos = reference.child("produtos");
+
+       /* DatabaseReference produtos = reference.child("produtos");
         Produto produto = new Produto();
          produto.setName("Lg");
          produto.setDescriction("Smart tv");
          produto.setPrice(3000);
-
-         produtos.child("002").setValue(produto);
+         produtos.child("002").setValue(produto);*/
 
        /* DatabaseReference usuarios = reference.child("usuarios");
         Usuario usuario = new Usuario();
